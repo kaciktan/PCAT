4b172a4ca160e65741dc4a519011d8aa9ca4a228
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-09-03 16:51:07 +0300

    Template Engine Added

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const port = 80;
 
-const myLogger = (req,res,next)=>{
-    console.log(req.url);
-    console.log("Middleware Log 1");
-    next();//Middleware pipeline a devam eder
-};
 
-//app.use(myLogger);
+
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+//MIDDLEWARES 
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/',(req,res)=>{
-  // res.sendFile(path.resolve(__dirname,'about.html'))
+  res.render('index');
 });
+
+app.get('/about',(req,res)=>{
+  res.render('about');
+});
+
+
+app.get('/add',(req,res)=>{
+  res.render('add');
+});
+
 
 app.listen(port,()=>{
     console.log(`Sunucu ${port} Portunda Başlatıldı..`);
 });
+
+// ÖRNEK MİDDLEWARE
+// const myLogger = (req,res,next)=>{
+//     console.log(req.url);
+//     console.log("Middleware Log 1");
+//     next();//Middleware pipeline a devam eder
+// };
+//app.use(myLogger);
