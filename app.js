c62c6bbe178b9471488d71fb0b894c93c92ed84a
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-09-04 21:25:44 +0300

    Photo Edit And Delete

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 
 const Photo = require('./models/Photo');
 
 const app = express();
 const port = 80;
-
 
 //Veritabanına bağlandık
 mongoose.connect('mongodb://localhost/pcat-test-db', {
@@ -16,40 +18,84 @@ mongoose.connect('mongodb://localhost/pcat-test-db', {
   useUnifiedTopology: true,
 });
 
-
 //TEMPLATE ENGINE
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
-//MIDDLEWARES 
+//MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true})); // bodyparser yerine kullanılıyor -> urldeki datayı okuyor
+app.use(express.urlencoded({ extended: true })); // bodyparser yerine kullanılıyor -> urldeki datayı okuyor
 app.use(express.json()); //urldeki datayı json a çeviriyor
+app.use(fileUpload()); //file upload için middleware dahil ediyoruz.
+app.use(methodOverride('_method',{
+  methods:['POST','GET']
+}));
 
 //ROUTES
-app.get('/', async(req,res)=>{
+app.get('/', async (req, res) => {
   const photos = await Photo.find({});
-  res.render('index',{
-    photos
+  res.render('index', {
+    photos,
   });
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
   res.render('about');
 });
 
-
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', async (req,res)=>{
-  await Photo.create(req.body);
+app.get('/photo/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', { photo });
+});
+
+app.post('/photos', async (req, res) => {
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/edit/:id',async (req,res)=>{
+  const photo= await Photo.findById(req.params.id);
+  res.render('edit',{photo});
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photo/${req.params.id}`)
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
   res.redirect('/');
 });
 
 
-app.listen(port,()=>{
-    console.log(`Sunucu ${port} Portunda Başlatıldı..`);
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} Portunda Başlatıldı..`);
 });
 
 // ÖRNEK MİDDLEWARE
